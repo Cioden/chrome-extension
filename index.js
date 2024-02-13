017c9d5c9d8f30a3bd71c31d336c73cf98d8f31a
Cioden <110314589+Cioden@users.noreply.github.com>
2024-02-13 13:28:06 +0800

    for initial deployment

--- index.js
+++ index.js
@@ -13,12 +13,11 @@ function renderLeads() {
     let listItems = ""
     for (let i = 0; i < myLeads.length; i++) {
         // listItems += "<li><a href = '" + myLeads[i] + "' target = '_blank'>" + myLeads[i] + "</a></li>"
-        listItems += `
-            <li>
-                <a href = '${myLeads[i]}' target = '_blank'>
-                    ${myLeads[i]}
-                </a>
-            </li>`
+        listItems += `<li>
+                        <a href = '${myLeads[i]}' target = '_blank'>
+                            ${myLeads[i]}
+                        </a>
+                    </li>`
     // ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
     // const li = document.createElement("li")
     // li.textContent = myLeads[i]
